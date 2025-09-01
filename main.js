4f107494530305132494200d0dc1d2c94d396403
Michael Rommel <melorm@posteo.de>
2025-09-01 13:56:32 +0200

    Kino Kasse

--- main.js
+++ main.js
@@ -1,39 +1,17 @@
-function main(){
-    let input = document.getElementById("inputColor")
-    let color = input.value
-    let lowerColorName = color.toLowerCase()
-
-if(lowerColorName == "grün"){
-    alert("Du darfst fahren")
-} else if(lowerColorName == "orange"){
-    alert("Du darfst anfahren oder musst bremsen")
-} else if(lowerColorName == "rot"){
-    alert("Du musst halten")
-} else {
-    alert("Keine gültige Ampel-Farbe eingetragen")
-}
-}
-
-/* Ausgelagerte (Globale) Variablen
-
-    let input
-    let color
-    let lowerColorName
+//Kino Kasse
 
 function main(){
-    input = document.getElementById("inputColor")
-    color = input.value
-    lowerColorName = color.toLowerCase()
+    let input = document.getElementById("age")
+    let age = input.value
 
-if(lowerColorName == "grün"){
-    alert("Du darfst fahren")
-} else if(lowerColorName == "orange"){
-    alert("Du darfst anfahren oder musst bremsen")
-} else if(lowerColorName == "rot"){
-    alert("Du musst halten")
-} else {
-    alert("Keine gültige Ampel-Farbe eingetragen")
-}
-}
 
-*/
+    if(age < 0 || age > 110){
+        alert("Bitte trage ein gültiges Alter ein")
+    } else if(age <= 6 || age >= 60){
+        alert("Preis beträgt 4€")
+    } else {
+        alert("Preis beträgt 6.50€")
+    }
+
+
+}
