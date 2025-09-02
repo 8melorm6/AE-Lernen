7c00708c439516900c73a3a15159552698a56968
Michael Rommel <melorm@posteo.de>
2025-09-02 08:22:36 +0200

    //Switch-Statements

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-//Blitzer
+//Switch-Statements
 
 function main(){
-    let inputMaxSpeed = document.getElementById("maxSpeed")
-    let inputSpeed = document.getElementById("speed")
-    let inputDir = document.getElementById("dir")
 
-    let maxSpeed = Number(inputMaxSpeed.value)
-    let speed = Number(inputSpeed.value)
-    let isFacing = inputDir.checked
+    let product = "Apfel";
 
-    let maxSpeedWithOffset = Number(maxSpeed + ((maxSpeed/100)*10))
-    console.log(maxSpeedWithOffset)
+    switch(product){
 
-    if(speed > maxSpeedWithOffset && isFacing == true){
-        alert("Du wurdest geblitzt")
-    } 
+        case "Mehl":
+            console.log("Mehl kostet 1€");
+            break;
+        case "Milch":
+            console.log("Milch kostet 2€");
+            break;
+        case "Apfel":
+        case "Birne":
+            console.log("Früchte kosten 1€");
+            break;
+        default:
+            console.log("Für dieses Produkt konnte kein Preis ermittelt werden");
+    }
 
 }
+
+
+// Mit Integer
+/*
+function main(){
+
+    let product = 10;
+
+    switch(product){
+
+        case 1:
+            console.log("....kostet 1€");
+            break;
+        case 10:
+            console.log("....kostet 2€");
+            break;
+        case 15:
+        case 20:
+            console.log("....kosten 1€");
+            break;
+        default:
+            console.log("Für dieses Produkt konnte kein Preis ermittelt werden");
+    }
+
+}
+
+*/
