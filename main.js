474fd7d018344dadea8f3a6e8dec155aad5b4a14
Michael Rommel <melorm@posteo.de>
2025-09-02 09:36:16 +0200

    Schleifen

--- main.js
+++ main.js
@@ -1,50 +1,101 @@
-//Switch-Statements
+// Schleifen (Loops)
 
 function main(){
 
-    let product = "Apfel";
+// -----------------------------------------------------------------
+// ---------------------------for-----------------------------------
+// -----------------------------------------------------------------
 
-    switch(product){
+/*---------------------------------------------------------------------
+let names = ["Peter", "Jannick", "Maria"];  
+// Ein Array (Liste) mit drei Zeichenketten: Index 0="Peter", 1="Jannick", 2="Maria".
 
-        case "Mehl":
-            console.log("Mehl kostet 1€");
-            break;
-        case "Milch":
-            console.log("Milch kostet 2€");
-            break;
-        case "Apfel":
-        case "Birne":
-            console.log("Früchte kosten 1€");
-            break;
-        default:
-            console.log("Für dieses Produkt konnte kein Preis ermittelt werden");
+for (let i = 0; i < names.length; i++) {  
+    // Startwert: i = 0
+    // Bedingung: Schleife läuft solange i < names.length (3).
+    // i wird nach jedem Durchlauf um 1 erhöht.
+
+    console.log("Index: " + i + " Name: " + names[i]);  
+    // Gibt eine Zeichenkette aus, die Index und Wert im Array zeigt.
+    // Bei i=0 -> "Index: 0 Name: Peter"
+    // Bei i=1 -> "Index: 1 Name: Jannick"
+    // Bei i=2 -> "Index: 2 Name: Maria"
+}
+---------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------
+let revenue = [500, 300, 200, 599, 834, 573, 293, 482];  
+// Ein Array mit Zahlen (Umsätze).
+
+let totalRevenue = 0;  
+// Variable für die Summe aller Umsätze, Startwert 0.
+
+for (let i = 0; i < revenue.length; i++) {  
+    // Startwert: i = 0
+    // Bedingung: Schleife läuft solange i < revenue.length (8).
+    // i wird nach jedem Durchlauf um 1 erhöht.
+
+    totalRevenue += revenue[i];  
+    // Kurzform für: totalRevenue = totalRevenue + revenue[i]
+    // Der aktuelle Umsatzwert wird zur Gesamtsumme addiert.
+
+    console.log(totalRevenue);  
+    // Gibt die Zwischensumme nach jeder Addition aus.
+}
+console.log("Total-Revenue: " + totalRevenue);  
+// Am Ende wird die Gesamtsumme aller Umsätze ausgegeben.
+---------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------
+// Array mit mehreren Objekten
+let cars = [
+    {
+        "brand": "Mercedes",
+        "age": 10
+    },
+    {
+        "brand": "VW",
+        "age": 5
+    },
+    {
+        "brand": "Toyota",
+        "age": 3
     }
+]
 
+// Geht alle Elemente des Arrays durch.
+    // Bei jedem Durchlauf ist 'car' ein komplettes Objekt.
+    // 1. Durchlauf: car = { brand: "Mercedes", age: 10 }
+    // 2. Durchlauf: car = { brand: "VW", age: 5 }
+    // 3. Durchlauf: car = { brand: "Toyota", age: 3 }
+for(let car of cars){
+
+    if(car.brand == "Mercedes"){
+        console.log(car);
+    } else {
+        console.log("Auto ist kein Mercedes");
+    } 
+}
+---------------------------------------------------------------------*/
+
+// -----------------------------------------------------------------
+// ------------------------(do)while------------------------------------
+// -----------------------------------------------------------------
+
+/*-----------------------------------------------------------------
+let counter = 0;
+
+while (counter < 100){
+    console.log(counter);
+    counter++;
 }
 
 
-// Mit Integer
-/*
-function main(){
+//  do {
+//    console.log(counter);
+//  } while(counter <100);
 
-    let product = 10;
 
-    switch(product){
-
-        case 1:
-            console.log("....kostet 1€");
-            break;
-        case 10:
-            console.log("....kostet 2€");
-            break;
-        case 15:
-        case 20:
-            console.log("....kosten 1€");
-            break;
-        default:
-            console.log("Für dieses Produkt konnte kein Preis ermittelt werden");
-    }
+-----------------------------------------------------------------------*/
 
 }
-
-*/
