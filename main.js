2c9b4d56362d875a671a48ea7593ab767e0a6d25
Michael Rommel <melorm@posteo.de>
2025-10-02 12:08:58 +0200

    Klassen

--- main.js
+++ main.js
@@ -1,55 +1,16 @@
-// Aufgabe 2 Teil 2
+// Klassen
 
-/* 
-Erstelle ein Array mit 5 Studenten.
-Jeder Student besteht aus 'Name', 'Matrikelnummer', 'NC'
-*/
 
-let studenten = [
+    class Student {
 
-]
-
-/* 
-Erstelle eine Funktion, die mit Hilfe einer Schleife
-jeden Studenten in die Konsole ausgibt und den NC
-aller Studenten summiert, um einen Durchschnitt zu erhalten.
-(Durchschnitt = Totale Summe / Anzahl)
-*/
-
-function ausgabeUndDurchschnitt() {
-
-    let totalNC = 0;
-
-    for (let i = 0; i < studenten.length; i++) {
-
-        let student = studenten[i];
-        console.log(student);
+        constructor(name, matrikel, nc){
+            this.name = name;
+            this.matrikel = matrikel;
+            this.nc = nc;
+        }
         
-        totalNC += student.NC;
-
     }
 
-    let averageNC = totalNC / studenten.length;
-    console.log("NC Durchschnitt: " + averageNC);
 
-}
-
-function addStudent(){
-
-    let name = document.getElementById("inputName").value;
-    let matrikel = parseInt(document.getElementById("inputMatrikel").value);
-    let nc = parseFloat(document.getElementById("inputNC").value);
-    
-    let student = {
-        "Name": name,
-        "Matrikelnummer": matrikel,
-        "NC": nc
-    }
-
-    studenten.push(student);
-    console.log(studenten);
-
-}
-
-
-// Hinweis: Funktion aufrufen (invoke)
+let student = new Student("Michi",1,1)
+console.log(student);
